a0182b758185e3643a8fe7ddf117c2eb0616ee16
Кайрат Смаков <@itstep.krg>
2022-10-03 21:04:31 +0600

    Second commit

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import datetime
 from time import sleep
 
 def my_decorator(func):
-    start_date = datetime.datetime.now()
-    func()
-    end_date = datetime.datetime.now()
-    diff = end_date - start_date
-    print(f'The function ({func.__name__}) elapsed: {diff}')
+    def wrapper(*args,**kwargs):
+        start_date = datetime.datetime.now()
+        func(*args,**kwargs)
+        end_date = datetime.datetime.now()
+        diff = end_date - start_date
+        print(f'The function ({func.__name__}) elapsed time: {diff}')
+    
+    return wrapper
 
 @my_decorator
-def test_function():
+def test_function(x, y):
     print("I am run!")
     sleep(5)
+    print(x + y)
+
+test_function(5, 5)
